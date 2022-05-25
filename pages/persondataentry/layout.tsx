5857e98ea89d1f1b22cde8d282cd56d7d044fd2a
Niques928 <chiniquejoseph@gmail.com>
2022-05-25 16:36:33 +0300

    Fixed: sidebar

--- pages/persondataentry/layout.tsx
+++ pages/persondataentry/layout.tsx
@@ -1,22 +1,28 @@
 import { PersonDataEntrySideBar } from "../../components/SideBars/PersonDataEntrySideBar";
 import { PersonDataEntryHeader } from "../../components/Headers/PersonDataEntryHeader";
-import Table from "../../components/Table/Table"
-import AccountMain from "../../components/SummaryBar/AccountMain"
-import BreadCrumb from "../../components/BreadCrumb"
-import useStyles from '../styles';
+import Table from "../../components/Table/Table";
+import AccountMain from "../../components/SummaryBar/AccountMain";
+import BreadCrumb from "../../components/BreadCrumb";
+import useStyles from "../styles";
 
-export function Layout({ children, breadcrumb }: {children: any, breadcrumb: string}) {
+export function Layout({
+  children,
+  breadcrumb,
+}: {
+  children: any;
+  breadcrumb: string;
+}) {
   const classes = useStyles();
 
   return (
     <div className={classes.layout}>
-      <PersonDataEntrySideBar/>
+      <PersonDataEntrySideBar />
       <div className={classes.content}>
         <PersonDataEntryHeader />
         <AccountMain />
-        <BreadCrumb breadcrumbs={['Person Data Entry', breadcrumb]} />
-        <main>{ children }</main>
+        <BreadCrumb breadcrumbs={["Person Data Entry", breadcrumb]} />
+        <main>{children}</main>
       </div>
     </div>
-  )
+  );
 }
