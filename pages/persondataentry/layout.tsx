93f155cefd98a8d4fd9ee91516d38b369b6a269a
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2022-06-17 02:05:18 +0300

    fixed:sidebarLinks

--- pages/persondataentry/layout.tsx
+++ pages/persondataentry/layout.tsx
@@ -4,10 +4,10 @@ import AccountMain from "../../components/SummaryBar/AccountMain";
 import BreadCrumb from "../../components/BreadCrumb";
 import useStyles from "../styles";
 import SideBar from "../components/SideBar";
-import {BsFillPersonFill, BsPhoneFill} from "react-icons/bs";
-import {HiOutlineMailOpen} from "react-icons/hi";
-import {IoMdShare} from "react-icons/io";
-import {FaClipboardList, FaHome, FaMap, FaSuitcase} from "react-icons/fa";
+import { BsFillPersonFill, BsPhoneFill } from "react-icons/bs";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { IoMdShare } from "react-icons/io";
+import { FaClipboardList, FaHome, FaMap, FaSuitcase } from "react-icons/fa";
 
 export function Layout({
   children,
@@ -17,79 +17,79 @@ export function Layout({
   breadcrumb: string;
 }) {
   const classes = useStyles();
-    const arrowNavigation = [
-        {
-            link: "/persondataentry/interactions",
-            icon: <BsFillPersonFill className="react-icon" />,
-            text: "Person Interactions",
-        },
-        {
-            link: "/persondataentry/electronicaddress",
-            icon: <HiOutlineMailOpen className="react-icon" />,
-            text: "Electronic Addresses",
-        },
-        {
-            link: "/persondataentry/affiliations",
-            icon: <IoMdShare className="react-icon" />,
-            text: "Affiliations",
-        },
-        {
-            link: "/persondataentry/locations",
-            icon: <FaMap className="react-icon" />,
-            text: "Locations",
-        },
-        {
-            link: "/persondataentry/phones",
-            icon: <BsPhoneFill className="react-icon" />,
-            text: "Phones",
-        },
-        {
-            link: "/persondataentry/othernames",
-            icon: <FaClipboardList className="react-icon" />,
-            text: "Other Names",
-        },
-        {
-            link: "/persondataentry/homeaddress",
-            icon: <FaHome className="react-icon" />,
-            text: "Home Address",
-        },
-        {
-            link: "/persondataentry/employment.tsx",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Employment",
-        },
-        {
-            link: "/persondataentry/employmenthistory",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Employment Id History",
-        },
-        {
-            link: "/persondataentry/employment.tsx",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Submissions",
-        },
-        {
-            link: "/persondataentry/employment.tsx",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Deductions",
-        },
-        {
-            link: "/persondataentry/employment.tsx",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Research",
-        },
-        {
-            link: "/persondataentry/employment.tsx",
-            icon: <FaSuitcase className="react-icon" />,
-            text: "Campuses",
-        },
-    ];
+  const arrowNavigation = [
+    {
+      link: "/persondataentry/interaction",
+      icon: <BsFillPersonFill className="react-icon" />,
+      text: "Person Interactions",
+    },
+    {
+      link: "/persondataentry/electronicaddress",
+      icon: <HiOutlineMailOpen className="react-icon" />,
+      text: "Electronic Addresses",
+    },
+    {
+      link: "/persondataentry/affiliations",
+      icon: <IoMdShare className="react-icon" />,
+      text: "Affiliations",
+    },
+    {
+      link: "/persondataentry/locations",
+      icon: <FaMap className="react-icon" />,
+      text: "Locations",
+    },
+    {
+      link: "/persondataentry/phones",
+      icon: <BsPhoneFill className="react-icon" />,
+      text: "Phones",
+    },
+    {
+      link: "/persondataentry/othernames",
+      icon: <FaClipboardList className="react-icon" />,
+      text: "Other Names",
+    },
+    {
+      link: "/persondataentry/homeaddress",
+      icon: <FaHome className="react-icon" />,
+      text: "Home Address",
+    },
+    {
+      link: "/persondataentry/employment",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Employment",
+    },
+    {
+      link: "/persondataentry/employeeidhistory",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Employment Id History",
+    },
+    {
+      link: "/persondataentry/submissions",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Submissions",
+    },
+    {
+      link: "/persondataentry/deductions",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Deductions",
+    },
+    {
+      link: "/persondataentry/research",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Research",
+    },
+    {
+      link: "/persondataentry/campus",
+      icon: <FaSuitcase className="react-icon" />,
+      text: "Campuses",
+    },
+  ];
 
   return (
     <div className={classes.layout}>
-        <SideBar options={arrowNavigation} title={'Person Data Entry'}/>
+      <SideBar options={arrowNavigation} title={"Person Data Entry"} />
 
-        <div className={classes.content}>
+      <div className={classes.content}>
         <PersonDataEntryHeader />
         <AccountMain />
         <BreadCrumb breadcrumbs={["Person Data Entry", breadcrumb]} />
